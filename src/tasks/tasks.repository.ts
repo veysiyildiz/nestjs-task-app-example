1f26cf5fd53e148659452db7d084d40d4bdf013b
Veysi YILDIZ <veysiyildiz@gmail.com>
2023-09-24 02:01:31 +0300

    task service tests

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -3,7 +3,7 @@ import { EntityRepository, Repository } from 'typeorm';
 import { Task } from './task.entity';
 import { TaskStatus } from './task-status.enum';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
-import { User } from 'src/auth/user.entity';
+import { User } from '../auth/user.entity';
 import { Logger, InternalServerErrorException } from '@nestjs/common';
 
 @EntityRepository(Task)
